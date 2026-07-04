2d167e462049ced29b8f52c71725b82f72c9dc53
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2026-07-04 15:26:52 +0300

    fix: simplify widget test for CI compatibility

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Basic sanity check', () {
-    // Test بسيط مش محتاج Flutter environment
     expect(1 + 1, equals(2));
   });
+
+
 }
